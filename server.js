58b7967c0e898fb22d9539f6f46b13f7cd0d46ae
tucanolis <arthur.sinmc@gmail.com>
2024-04-11 10:12:43 -0300

    chore: create routes

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 import http from "http";
 
+const PORT = 3000;
+
+const rotas = {
+    "/": "curso de node.js"
+}
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/plain"});
-    res.end("curso de node.js")
+    res.end(rotas[res.url]);
 });
 
 server.listen(3000, () => {
